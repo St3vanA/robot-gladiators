e6e192e3d9e4ace6dd7affd0bfeb5ef9cabce047
Esteban Arredondo <st3van1193@gmail.com>
2021-09-20 10:13:45 -0700

    javascript variables

--- js/game.js
+++ js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("Bender Bending Rodriguez");
+// Note the lack of quotation marks around plaerName console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10+10);
+// What is this?
+console.log("Our robot's name is " + playerName)
+function fight(); {
+    window.alert("This is an alert! JavaScript is running")
+}
+// fight();
