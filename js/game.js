bef7d00144e6f37c9c34b791192cab0ef711e916
Esteban Arredondo <st3van1193@gmail.com>
2021-09-28 16:02:12 -0700

    start and end game functions

--- js/game.js
+++ js/game.js
@@ -70,27 +70,48 @@ var fight = function(enemyName) {
 };
 
 // fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) {
-  // if player is still alive, keep fighting
-  if (playerHealth > 0) {
-    // let player know what round they are in
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+var startGame = function() {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      
 
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      var pickedEnemyName = enemyNames[i];
+      
+      enemyHealth = 50;
 
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
 
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
   }
-  // if player isn't alive, stop the game
+
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
+  endGame();
+};
+startGame();
+var endGame = function() {
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survied the game! YOu now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
 }
+else {
+  window.alert("Thank you playing Robot Gladiators! Come back soon!");
+}
+  window.alert("The game is now ended. Let's see how you did!");
+};
+};
+startGame();
